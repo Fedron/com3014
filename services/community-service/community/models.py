d83e52b0301f505ce983e9303c5b31e2ead82638
ShieldedTurtle <129774817+ShieldedTurtle@users.noreply.github.com>
2025-03-15 14:11:41 +0000

    barebones community service implementation

--- services/community-service/community/models.py
+++ services/community-service/community/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
+
 class Community(models.Model):
     name = models.CharField(max_length=255)
+    desc = models.TextField()
--- services/community-service/community/urls.py
+++ services/community-service/community/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import community_list_create, community_details
 
 app_name = 'community'
 
 urlpatterns = [
-
+    path('', community_list_create, name = 'community-list-create'),
+    path('<int:community_id>/', community_details, name = 'community-details')
 ]
