7472ee969de4684e8063467637edc33ddd6ac7f4
Alex Morris <am04553@surrey.ac.uk>
2025-03-20 19:22:22 +0000

    Added Comment model

--- services/content-service/post/models.py
+++ services/content-service/post/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.CharField()
     dateCreated = models.DateTimeField(auto_now_add = True)
+    communityId = models.IntegerField()
+    userId = models.IntegerField()
     #Add once community branch merged
-    #community = models.ForeignKey(Community, null = False, blank = False, on_delete = models.CASCADE)
+
+class Comment(models.Model):
+    userId = models.IntegerField()
+    replyTo = models.ForeignKey()
+    description = models.CharField()
+    dateCreated = models.DateTimeField(auto_now_add = True)
