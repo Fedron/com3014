a98aa86aaea7c2815fbb5a7afe88c4f48be99b74
Alex Morris <morrisalex84@gmail.com>
2025-04-01 09:51:45 +0100

    Ammended signals to invalidate cache when changes made to posts and comments

--- services/content-service/post/signals.py
+++ services/content-service/post/signals.py
@@ -5,8 +5,16 @@ from django.core.cache import cache
 
 @receiver([post_save, post_delete], sender=Post)
 def invalidate_post_cache(sender, instance, **kwargs):
-    #Invalidate post list caches when a post is created, updated or destroyed
+    #Invalidate post caches when a post is created, updated or destroyed
     print("Clearing post cache")
 
     #Clear post list caches
-    cache.delete_pattern('*post_list*')
+    cache.delete_pattern('*post_detail*')
+
+@receiver([post_save, post_delete], sender=Post)
+def invalidate_comment_cache(sender, instance, **kwargs):
+    #Invalidate comment caches when a comment is created, updated or destroyed
+    print("Clearing comment cache")
+
+    #Clear post list caches
+    cache.delete_pattern('*comment_detail*')
