4cb07301b3e1a8611e01dc8a9c253dc4501e9b39
Fedron <40535546+Fedron@users.noreply.github.com>
2025-04-28 16:43:36 +0200

    Add Prometheus stack to k8s cluster (#14)
    Add Prometheus stack to k8s cluster

--- api-gateway/src/main.rs
+++ api-gateway/src/main.rs
@@ -1,10 +1,11 @@
 use std::sync::Arc;
 
 use aide::{axum::ApiRouter, openapi::OpenApi, transform::TransformOpenApi};
-use axum::Extension;
+use axum::{Extension, middleware};
 use axum_reverse_proxy::ReverseProxy;
 use docs::docs_routes;
 use error::AppError;
+use metrics::{start_metrics_server, track_metrics};
 use state::AppState;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -12,10 +13,11 @@ mod auth;
 mod docs;
 mod error;
 mod events;
+mod metrics;
 mod state;
 
 #[tokio::main]
-async fn main() -> Result<(), AppError> {
+async fn main() {
     dotenvy::dotenv().ok();
 
     tracing_subscriber::registry()
@@ -29,14 +31,20 @@ async fn main() -> Result<(), AppError> {
     aide::generate::on_error(|error| tracing::error!("{error}"));
     aide::generate::extract_schemas(true);
 
-    let state = AppState::from_dotenv()?;
+    tokio::join!(start_main_server(), start_metrics_server());
+}
+
+async fn start_main_server() {
+    let state = AppState::from_dotenv().unwrap();
     let mut api = OpenApi::default();
 
     let community_service = std::env::var("COMMUNITY_SERVICE_HOST")
-        .map_err(|_| AppError::EnvironmentVariable("COMMUNITY_SERVICE_HOST"))?;
+        .map_err(|_| AppError::EnvironmentVariable("COMMUNITY_SERVICE_HOST"))
+        .unwrap();
 
     let content_service = std::env::var("CONTENT_SERVICE_HOST")
-        .map_err(|_| AppError::EnvironmentVariable("CONTENT_SERVICE_HOST"))?;
+        .map_err(|_| AppError::EnvironmentVariable("CONTENT_SERVICE_HOST"))
+        .unwrap();
 
     let app = ApiRouter::new()
         .nest_api_service("/docs", docs_routes(state.clone()))
@@ -56,24 +64,22 @@ async fn main() -> Result<(), AppError> {
             "/proxied/content",
             &format!("http://{content_service}"),
         ))
+        .route_layer(middleware::from_fn(track_metrics))
         .with_state(state);
 
     let host = std::env::var("HOST").unwrap_or("127.0.0.1".to_string());
     let port = std::env::var("PORT").unwrap_or("3000".to_string());
     let addr = format!("{host}:{port}");
 
-    let listener = tokio::net::TcpListener::bind(&addr)
-        .await
-        .map_err(|_| AppError::CantListen(addr))?;
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
 
     tracing::debug!("listening on http://{}", listener.local_addr().unwrap());
     tracing::debug!(
         "documentation available at http://{}/docs",
         listener.local_addr().unwrap()
     );
-    axum::serve(listener, app)
-        .await
-        .map_err(|e| AppError::Io(e.to_string()))
+
+    axum::serve(listener, app).await.unwrap();
 }
 
 fn api_docs(api: TransformOpenApi) -> TransformOpenApi {
--- infra/deploy_k8.sh
+++ infra/deploy_k8.sh
@@ -8,10 +8,9 @@ cleanup() {
 
 trap cleanup SIGINT
 
-minikube stop
 minikube start --cpus=4 --memory=4096 --disk-size=20g
 
-eval $(minikube docker-env)
+eval $(minikube -p minikube docker-env)
 
 echo "Building auth-service docker image"
 docker build -t auth-service:latest -f services/auth-service/Dockerfile .
@@ -30,10 +29,21 @@ docker build -t community-service:latest -f services/community-service/Dockerfil
 echo "Building content-service docker image"
 docker build -t content-service:latest -f services/content-service/Dockerfile .
 
+echo "Deploying ANTTO"
 cd ./infra/helm/umbrella-chart
 helm dependency update
 helm uninstall antto
 helm install antto ./
+
+echo "Deploying observability stack"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
+
+kubectl patch svc prometheus-grafana -n monitoring -p '{"spec": {"type": "LoadBalancer"}}'
+
+kubectl --namespace monitoring get secrets prometheus-grafana -o jsonpath="{.data.admin-password}" | base64 -d ; echo
 
 echo "Starting Minikube Tunnel... Press Ctrl+C to stop."
 minikube tunnel &
